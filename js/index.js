cb7f97129ad2de16acac1a0ec50f43e1043502d3
pengliheng <pengliheng@qq.com>
2017-05-14 14:22:36 -0700

    第一次用angular-ui-router搭建好切换页面，不是很完美，ui-view里 面的内容无法用js修改，甚至还不知道在哪修改，每个子页面的控制器没写好，但是基本外观是实现了^C接下来我要做animate

--- js/index.js
+++ js/index.js
@@ -1,15 +1,55 @@
-//angular路由设置
-angular.module('routingDemoApp',['ngRoute'])
-.config(['$routeProvider', function($routeProvider){
-    $routeProvider
-    .when('/',{templateUrl:'view/home.html'})
-    .when('/second',{templateUrl:'view/second.html'})
-    .when('/third',{templateUrl:'view/third.html'})
-    .otherwise({redirectTo:'/'});
-}]);
+//angular-route路由设置
+// angular.module('routingDemoApp',['ngRoute'])
+// .config(['$routeProvider', function($routeProvider){
+//     $routeProvider
+//     .when('/',{templateUrl:'view/home.html'})
+//     .when('/second',{templateUrl:'view/second.html'})
+//     .when('/third',{templateUrl:'view/third.html'})
+//     .otherwise({redirectTo:'/'});
+// }]);
+
+
+
+//angualr-ui-route 路由设置
+angular.module('uirouterdemo', ['ui.router'])
+.config(function($stateProvider) {
+	var homeState = {
+		name: 'home',
+		url: '',
+		templateUrl: 'view/home.html'
+	}
+
+	var secondState = {
+		name: 'second',
+		url: '/second',
+		templateUrl: 'view/second.html'
+	}
+
+	var thirdState = {
+		name: 'third',
+		url: '/third',
+		templateUrl: 'view/third.html'
+	}
+
+	$stateProvider.state(homeState);
+	$stateProvider.state(secondState);
+	$stateProvider.state(thirdState);
+})
+
+
+
+
+
+
+
+
 
 
 $("ul.nav li").click(function(){
+    $('ul.nav li').removeClass("on"); 
+    $(this).addClass("on"); 
+})
+$(".context-mod").click(function(){
     $('ul.nav li').removeClass("on"); 
     $(this).addClass("on"); 
 })
